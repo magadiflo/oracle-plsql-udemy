cf6e80fe92dde9709590930fc78e36bb58ed6ebb
Martín <magadiflo@gmail.com>
2022-07-05 11:41:58 -0500

    Bloques anidados

---
+++ 10.bloque_anidados.sql
@@ -0,0 +1,13 @@
+-- 10. bloque_anidados
+SET SERVEROUTPUT ON;
+
+BEGIN
+    dbms_output.put_line('Primer bloque');
+    
+    DECLARE
+        x NUMBER(2) := 10;
+    BEGIN
+        dbms_output.put_line(x);
+    END;
+
+END;
