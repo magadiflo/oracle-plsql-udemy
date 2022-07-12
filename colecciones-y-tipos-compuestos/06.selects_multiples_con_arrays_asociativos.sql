b2895276588293ae90ffcb95331713a5254c182a
Martín <magadiflo@gmail.com>
2022-07-12 10:21:23 -0500

    Selects múltiples con arrays asociativos

---
+++ colecciones-y-tipos-compuestos/06.selects_multiples_con_arrays_asociativos.sql
@@ -0,0 +1,21 @@
+--06. Selects múltiples con arrays asotiativoss
+SET SERVEROUTPUT ON;
+
+DECLARE
+    TYPE departamentos IS TABLE OF
+        departments%ROWTYPE
+    INDEX BY PLS_INTEGER;
+    
+    depts departamentos;
+BEGIN
+    FOR i IN 1..10 LOOP
+        SELECT * 
+            INTO depts(i)
+        FROM departments
+        WHERE department_id = i * 10;
+    END LOOP;
+    
+    FOR i IN depts.FIRST..depts.LAST LOOP
+        dbms_output.put_line(depts(i).department_name);
+    END LOOP;    
+END;
